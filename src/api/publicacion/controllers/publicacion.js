ecc90a57aa4e41b018aa049ef14b195bab9d0f5c
Julián Gómez Suárez <18161126@itoaxaca.edu.mx>
2022-12-03 16:43:49 -0600

    First try

---
+++ src/api/publicacion/controllers/publicacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * publicacion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::publicacion.publicacion');
